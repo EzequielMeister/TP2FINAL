50513d1a820e813874f69bb9432fcd51958a4c79
Ezequiel <ezee7771@gmail.com>
2024-11-13 21:37:14 -0300

    Agregando mas logica..

--- services/Service.js
+++ services/Service.js
@@ -12,6 +12,31 @@ class Service {
       throw error;
     }
   };
+  getPalabrasApiService = async (cantidad) => {
+    try {
+      const data = this.model.getFrase();
+      const frase = data.map((palabra) => palabra.palabra)
+
+      const api= await fetch(`https://texto.deno.dev/palabras?cantidad=${cantidad}`)
+      const apiJson=await api.json()
+
+      return [...apiJson.palabras, ...frase];
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  createService = async (palabra) => {
+    try {
+      if (!palabra) throw new Error("Tenes que ingresar una palabra");
+
+      const frase = this.model.create(palabra)
+      return frase;
+
+    } catch (error) {
+      throw error;
+    }
+  };
 
 }
 
